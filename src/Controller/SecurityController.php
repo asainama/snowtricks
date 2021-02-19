ace6b60270137e06213271044fb31d2f3590dd16
sainama <sainama@economiedenergie.fr>
2021-02-19 10:24:38 +0100

    :hammer: Add register email
    Add register email part

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,18 +3,21 @@
 namespace App\Controller;
 
 use LogicException;
+use App\Form\UserType;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/register", name="app_register", methods={"GET","POST"})
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, TokenGeneratorInterface $token, \Swift_Mailer $mailer): Response
     {
         $form  = $this->createForm(UserType::class);
         $form->handleRequest($request);
@@ -24,9 +27,24 @@ class SecurityController extends AbstractController
             $user->setPassWord($passwordEncoder->encodePassword($user, $plainPassword));
             $user->setCreatedAt(new \DateTime('NOW'));
             $user->setRoles(['ROLE_USER']);
+            $user->setActivationToken($token->generateToken());
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
+
+            $message = (new \Swift_Message('Activation de votre compte'))
+            ->setFrom('ksoulkaiser@gmail.com')
+            ->setTo($user->getEmail())
+            ->setBody(
+                $this->renderView(
+                    'emails/activation.html.twig',
+                    [
+                        'token' => $user->getActivationToken()
+                    ],
+                    'text/html'
+                )
+            );
+            $mailer->send($message);
             $this->addFlash('success', 'User succesfully created');
             return $this->redirectToRoute('app_home');
         }
@@ -52,5 +70,24 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/activation/{token}", name="app_activation", methods={"GET"})
+     */
+    public function activation($token, UserRepository $userRepository): Response
+    {
+        $user= $userRepository->findOneBy(['activation_token' => $token]);
+        if (!$user) {
+            throw $this->createNotFoundException('Cet utilisateur n\'existe pas');
+        }
+        $user->setActivationToken(null);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Vous avez bien activé votre compte');
+
+        return $this->redirectToRoute('app_home');
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="`user`")
- * @UniqueEntity(email)
+ * @UniqueEntity("email")
  */
 class User extends UserRepository
 {
