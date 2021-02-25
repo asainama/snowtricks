13fad721588a9ec21524ba4b4bad4abbc69fd47d
sylva@DESKTOP-V5L4KT7 <sainama@economiedenergie.fr>
2021-02-25 23:32:43 +0100

    :monocle_face: Update form
    Update form js

--- assets/js/form.js
+++ assets/js/form.js
@@ -24,13 +24,16 @@ if (alertClose !== null) {
 
 var addVideo = document.querySelectorAll('.form__group__video__add')
 var deleteVideo = document.querySelectorAll('.form__group__video__delete')
-const parentDiv = document.querySelector('#videos__list')
+var addImage = document.querySelectorAll('.form__group__image__add')
+var deleteImage = document.querySelectorAll('.form__group__image__delete')
+var parentDivVideo = document.querySelector('#video__list')
+var parentDivImage = document.querySelector('#image__list')
 
 if (addVideo !== null) {
     addVideo.forEach(video => {
         video.addEventListener('click', function(e){
             e.preventDefault()
-            addForm(video)
+            addForm(parentDivVideo,'video')
         })
     });
 }
@@ -39,37 +42,75 @@ if (deleteVideo !== null) {
     deleteVideo.forEach(video => {
         video.addEventListener('click', function(e){
             e.preventDefault()
-            deleteForm(video)
+            deleteForm(video,'video')
         })
     });
 }
 
-function deleteForm(video){
-    console.log(video.getAttribute('data-id'))
-    var div = document.querySelector('div[data-delete="'+video.getAttribute('data-id')+'"]')
-    parentDiv.removeChild(div)
+if (addImage !== null) {
+    addImage.forEach(image => {
+        image.addEventListener('click', function(e){
+            e.preventDefault()
+            addForm(parentDivImage,'image')
+        })
+    });
 }
 
-function addForm(video) {
-    var index = document.querySelectorAll('.form__group__video__item').length
-    var prototype = parentDiv.getAttribute('data-prototype');
+if (addImage !== null) {
+    deleteImage.forEach(image => {
+        image.addEventListener('click', function(e){
+            e.preventDefault()
+            deleteForm(image,'image')
+        })
+    });
+}
+
+
+function deleteForm(item,type){
+    var div = document.querySelector('div[data-delete="'+item.getAttribute('data-id')+'"]')
+    document.querySelector('#'+type+'__list').removeChild(div)
+    checkLength(type)
+}
+
+function addForm(parentDiv,type) {
+    checkLength(type)
+    var index = document.querySelectorAll('.form__group__'+type+'__item').length
     var label = parentDiv.getAttribute('data-label')
+    var id = parentDiv.getAttribute('data-id')
+    id = id.replace(/__name__/g,index);
     label = label.replace(/__name__/g,index)
     var input = parentDiv.getAttribute('data-input')
     input = input.replace(/__name__/g,index)
     var div = document.createElement('div');
-    div.className = "form__group__video__item"
+    div.className = 'form__group__'+type+'__item'
+    div.setAttribute('data-delete', id)
     div.innerHTML = label
     var divBody = document.createElement('div')
     divBody.className = "form__group__action"
     divBody.innerHTML = input
     var a = document.createElement('a')
-    a.className = 'form__group__video__delete'
-    a.innerHTML= 'X'
+    a.className = 'form__group__'+type+'__delete'
+    a.setAttribute('data-id',id)
+    a.innerText= 'X'
     a.onclick = function(){
-        this.deleteForm(this)
+        deleteForm(this,type)
     }
     divBody.appendChild(a)
     div.appendChild(divBody)
-    parentDiv.appendChild(div)
+    var btnAdd = document.querySelector('.form__group__'+type+'__add')
+    var divBefore = btnAdd.parentNode
+    parentDiv.insertBefore(div,divBefore)
+}
+
+function checkLength(type) {
+    var add = document.querySelector('.form__group__'+type+'__add')
+    var countDiv = document.querySelectorAll('.form__group__'+type+'__item').length
+    if (countDiv === 3) {
+        if (!add.classList.contains('btn__hide')){
+            console.log(countDiv === 3)
+            add.classList.add('btn__hide')
+        } else {
+            add.classList.remove('btn__hide')
+        }
+    }
 }
